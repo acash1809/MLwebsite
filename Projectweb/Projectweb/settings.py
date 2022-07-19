754b4769cae8ccb2f4c95d1913296a44fd4d336a
Akash Chaturvedi <akashchaturvedi797@gmail.com>
2022-07-19 21:26:24 +0530

    done

--- Projectweb/Projectweb/settings.py
+++ Projectweb/Projectweb/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-boqugiyj*lz3d@h+m7fhpoauldt*#+%_x3s0&1amq+769s7cn0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
